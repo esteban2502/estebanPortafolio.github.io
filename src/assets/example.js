c2088412fbdcd810e60fc0745373371fb67f6bb4
Juan Esteban <juanes9001@gmail.com>
2025-01-04 20:42:10 -0500

    about y education desarrollados

---
+++ src/assets/example.js
@@ -0,0 +1,27 @@
+
+const AboutMe = {
+    technologies: {
+        backend: [
+          "Java", "Spring Boot", "Spring", "JPA",
+          "Microsoft SQL Server", "MySQL", "Hibernate", "API REST",
+          "Arquitectura Monolitica"
+        ],
+        frontend: [
+          "HTML", "CSS",
+          "JavaScript","Bootstrap"
+        ],
+        others: [
+            "Git", "MER",
+            "DDL","DML","TCL"
+          ],
+        learning : [
+            "Docker","Kubernetes",
+            "Spring Security", "Spring Cloud", " Arquitectura de Microservicios"
+        ]  
+      },
+      contact : {
+        email : "juanes9001@gmail.com",
+        linkedin : "https://www.linkedin.com/in/juan-esteban-buritica-garcia/"
+      }
+};
+
